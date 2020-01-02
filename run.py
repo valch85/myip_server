45e13093b9b280516bbc085d57a4e6b202cbc2ba
valch85 <val@valch.name>
2020-01-02 16:57:11 +0200

    show X-Forwarded-For header as a client IP

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ class MyRequestHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         # Send message back to client
-        message = self.get_client_ip()
+        message = str(self.headers.get('X-Forwarded-For'))
         # Write content as utf-8 data
         self.wfile.write(bytes(message, "utf8"))
         return
